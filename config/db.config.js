728d26cddd3624bcb45b7f0b2a895ff54d310112
juanbarrios18 <juanf18889@gmail.com>
2020-05-07 00:00:35 +0200

    modify db connection

--- config/db.config.js
+++ config/db.config.js
@@ -3,10 +3,16 @@ require('dotenv').config()
 
 const mongodbConnection = (process.env.NODE_ENV === 'dev') ? "mongodb+srv://mongodbuser:bulletjournal1234567@bulletjournal-js68h.mongodb.net/bulletjournal?retryWrites=true&w=majority" : process.env.MONGODB_URI
 
-mongoose
-  .connect(mongodbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(x => { console.log(`Connected to MongoDB, DB name: ${x.connections[0].name}`) })
-  .catch(e => {
-    console.log(e)
+
+  mongoose
+  .connect(mongodbConnection, { useNewUrlParser: true })
+  .then(() => console.info(`Successfully connected to the database ${mongodbConnection}`))
+  .catch(error => console.error(`An error ocurred trying to connect to de database ${mongodbConnection}`, error))
+
+
+  process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+      console.log('Mongoose disconnected on app termination')
+      process.exit(0);
+    })
   })
-  
