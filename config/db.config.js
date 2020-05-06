15e00b5533a92c47ea622adb50f2bc31284b16d4
Juan <juanbarrios18@HP.home>
2020-05-06 22:55:20 +0200

    config enviroment variables

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-//const mongodbConnection = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/bullet-journal' : process.env.MONGODB_URI
+const mongodbConnection = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/bullet-journal' : process.env.MONGODB_URI
 
 mongoose
-  .connect("mongodb+srv://mongodbuser:bulletjournal1234567@bulletjournal-js68h.mongodb.net/bulletjournal?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongodbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => { console.log(`Connected to MongoDB, DB name: ${x.connections[0].name}`) })
   .catch(e => {
     console.log(e)
