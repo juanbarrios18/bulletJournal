faca5ee2264c502e843a4f87335acd96ac9a97fd
Juan <juanbarrios18@HP.home>
2020-05-06 22:19:13 +0200

    create procfile

--- config/db.config.js
+++ config/db.config.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 //const mongodbConnection = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/bullet-journal' : process.env.MONGODB_URI
 
 mongoose
-  .connect('mongodb://localhost:27017/bullet-journal', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb+srv://mongodbuser:bulletjournal1234567@bulletjournal-js68h.mongodb.net/bulletjournal?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => { console.log(`Connected to MongoDB, DB name: ${x.connections[0].name}`) })
   .catch(e => {
     console.log(e)
   })
+  
